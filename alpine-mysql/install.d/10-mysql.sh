caaa642fb5456a60e62423b711ea966e0b342bed
fyf <fyfcnc@hotmail.com>
2016-09-05 15:57:31 +0800

    更新mysql

--- alpine-mysql/install.d/10-mysql.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# @app      pluie/alpine-mysql
-# @author   a-Sansara https://git.pluie.org/pluie/docker-images
-
-apk --update add mysql mysql-client pwgen \
--- alpine-mysql/main.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# @app      pluie/alpine-mysql
-# @author   a-Sansara https://git.pluie.org/pluie/docker-images
-
-. /scripts/common.sh
-exec /usr/bin/mysqld --user=mysql --console
